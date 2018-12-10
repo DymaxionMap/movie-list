235a0c4f782a20866ffe1dff4d740a9f785e7f21
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-09 17:00:38 -0800

    Add watched button to movie list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const ListItem = ({ text }) => (
-  <li className='list-group-item'>{text}</li>
-);
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressed: false
+    };
+
+    this.click = this.click.bind(this);
+    this.buttonPressed = 'btn btn-success watched';
+    this.buttonUnpressed = 'btn btn-success watched unpressed';
+  }
+
+  click() {
+    this.setState({
+      pressed: !this.state.pressed
+    });
+  }
+
+  render() {
+    return (
+      <li className='list-group-item'>
+        {this.props.text}
+        <button className={this.state.pressed ? this.buttonPressed : this.buttonUnpressed} 
+          onClick={this.click}>
+          Watched
+        </button>
+      </li>
+    );
+  }
+};
+
+// const ListItem = ({ text }) => (
+//   <li className='list-group-item'>{text}<button className='btn btn-success watched unpressed'>Watched</button></li>
+// );
 
 export default ListItem;
